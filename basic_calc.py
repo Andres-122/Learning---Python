1cf7ff5360eca48f3f15936d36b5959bb991e804
Andres R <andress101013@gmail.com>
2024-01-20 09:20:49 -0500

    First commit basic_calc Script v.1.0 and 2

---
+++ basic_calc.py
@@ -0,0 +1,25 @@
+#Script title: Basic calculator v1
+'''Developer name Andres Rivera 
+Date 18-01-2024'''
+#inputs (dos variables) 
+num1 = 20
+num2 = 5
+#process
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1/num2
+#outputs
+print("::: BASIC CALC V1:::")
+print("numero 1", num1, type(num1))
+print("numero 2", num2, type(num2))
+print("El restultado de la suma es", add, type(add))
+print("El restultado de la resta es", sub, type(sub))
+print("El restultado de la multiplicación es", mul, type(mul))
+print("El restultado de la división es", div, type(div))
+print("::: BASIC CALC V2:::")
+print("la suma es ", num1 + num2)
+print("la resta es ", num1 - num2)
+print("la multiplicación es ", num1 * num2)
+print("la suma división ", num1/num2)
+
